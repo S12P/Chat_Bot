739e730990ca00bb78a02a7b27758ebcd50d7ca0
cegarciav <cegarcia2@uc.cl>
2020-03-27 19:41:52 +0100

    no https required v2

--- centraleprojet/static/js/script.js
+++ centraleprojet/static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var baseUrl = "https://centraleprojet.herokuapp.com/webhooks/rasa/webhook?token=mysecret";
+    var baseUrl = "centraleprojet.herokuapp.com/webhooks/rasa/webhook?token=mysecret";
 
     //---------------------------------- Add dynamic html bot content(Widget style) ----------------------------
     // You can also add the html content in html page and still it will work!
